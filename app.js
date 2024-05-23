8c5b1caca902de83add08044b2f0c0bed2cc47c3
scottmahony <158554772+scottmahony@users.noreply.github.com>
2024-05-23 14:01:26 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 // Configuration for the proxy middleware
 const options = {
