9a3dddb571b2b8db1d34adc603649cd429f9b136
scottmahony <158554772+scottmahony@users.noreply.github.com>
2024-06-13 14:29:50 -0700

    Update app.js

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ let privateKey;
 let certificate;
 
 try {
-  privateKey = fs.readFileSync('/etc/letsencrypt/live/blog.toptier.ventures/privkey.pem', 'utf8');
-  certificate = fs.readFileSync('/etc/letsencrypt/live/blog.toptier.ventures/fullchain.pem', 'utf8');
+  privateKey = fs.readFileSync('/etc/letsencrypt/live/toptier.ventures/privkey.pem', 'utf8');
+  certificate = fs.readFileSync('/etc/letsencrypt/live/toptier.ventures/fullchain.pem', 'utf8');
   console.log('Certificates loaded successfully');
 } catch (err) {
   console.error('Error loading certificates:', err);
@@ -35,6 +35,13 @@ app.use((err, req, res, next) => {
 });
 
 const httpsServer = https.createServer(credentials, app);
+
+// Redirect HTTP to HTTPS
+const http = require('http');
+http.createServer((req, res) => {
+  res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+  res.end();
+}).listen(80);
 
 httpsServer.listen(port, '0.0.0.0', () => {
   console.log(`Proxy server listening at https://blog.toptier.ventures`);
